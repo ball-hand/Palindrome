ce81aeb3f901470fd2694f104184a8cf14cb1846
Zero_Two <86848228+ball-hand@users.noreply.github.com>
2023-10-08 00:54:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    return str === str.split("").reverse().join("");
+  }
+
+  function checkPalindrome() {
+    const inputText = document.getElementById("inputText").value;
+    const result = document.getElementById("result");
+    if (isPalindrome(inputText)) {
+      result.innerText = `${inputText} is a palindrome!`;
+    } else {
+      result.innerText = `${inputText} is not a palindrome.`;
+    }
+  }
